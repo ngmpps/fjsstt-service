10b9d3460a1a0c0e54155289a894f52f1f5de4fd
iot365 <fstroh@gmail.com>
2016-07-20 16:50:07 +0200

    added ProblemSet resource (GET only)

--- src/main/java/at/ngmpps/fjsstt/rest/MyResource.java
+++ src/main/java/at/ngmpps/fjsstt/rest/MyResource.java
@@ -2,6 +2,8 @@ package at.ngmpps.fjsstt.rest;
 
 import at.ngmpps.fjsstt.factory.ModelFactory;
 import at.ngmpps.fjsstt.model.MyBean;
+import at.ngmpps.fjsstt.model.ProblemSet;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -39,6 +41,25 @@ public class MyResource {
 	@Produces(MediaType.APPLICATION_JSON)
 	public MyBean getMyBean(@PathParam("uri") String uri) {
 		return ModelFactory.createBean(uri);
+	}
+
+
+	/**
+	 * Method handling HTTP GET requests. The returned object will be sent to the
+	 * client as "application/json" media type.
+	 *
+	 * @return the ProblemSet, which can be parsed into a JSON Object
+	 * @param id: "srfg" to get the SRFG instance of the problem set, any other to get a dummy problem set
+	 */
+	@GET
+	@Path("/problem/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public ProblemSet getProblem(@PathParam("id") String id) {
+		if ("srfg".equals(id))
+			return ModelFactory.createSrfgProblemSet();
+		else {
+			return ModelFactory.createDummyProblemSet();
+		}
 	}
 
 }
