5d484f01f3ee5a7b9e3f595373189c362129abac
iot365 <fstroh@gmail.com>
2016-07-20 16:30:52 +0200

    added MyBean resource (GET only)

--- src/main/java/at/ngmpps/fjsstt/rest/MyResource.java
+++ src/main/java/at/ngmpps/fjsstt/rest/MyResource.java
@@ -1,7 +1,10 @@
 package at.ngmpps.fjsstt.rest;
 
+import at.ngmpps.fjsstt.factory.ModelFactory;
+import at.ngmpps.fjsstt.model.MyBean;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -22,4 +25,20 @@ public class MyResource {
 	public String getIt() {
 		return "Got it!";
 	}
+
+
+	/**
+	 * Method handling HTTP GET requests. The returned object will be sent to the
+	 * client as "application/json" media type.
+	 *
+	 * @return MyBean, which can be parsed into a JSON Object
+	 * @param uri
+	 */
+	@GET
+	@Path("/mybean/{uri}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public MyBean getMyBean(@PathParam("uri") String uri) {
+		return ModelFactory.createBean(uri);
+	}
+
 }
