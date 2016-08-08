9abeb5f14572f3c360987cd5b451083001c79ab2
gw <georg.weichhart@jku.at>
2016-08-08 09:07:05 +0200

    Remove Problems from Solutions to minimize data needed

--- src/main/java/at/ngmpps/fjsstt/model/problem/Solution.java
+++ src/main/java/at/ngmpps/fjsstt/model/problem/Solution.java
@@ -17,10 +17,6 @@ public class Solution implements Serializable {
 
 	private static final long serialVersionUID = -409137575428427517L;
 
-	/**
-	 * The problem to be solved.
-	 */
-	final FJSSTTproblem problem;
 
 	double objectiveValue;
 
@@ -56,6 +52,27 @@ public class Solution implements Serializable {
 	 * solution.
 	 */
 	final double[][] multipliers;
+	
+	/**
+	 * Nr of Jobs
+	 */
+	int jobs;
+	
+	/**
+	 * Nr of Machines
+	 */
+	int machines;
+	
+	/**
+	 * Nr of timeslots
+	 */
+	int timeslots;
+	
+	/**
+	 * Max Nr of Operations per Job
+	 */
+	int maxOperationsPerJob;
+	
 
 	public Solution() {
 		multipliers = new double[0][];
@@ -63,33 +80,32 @@ public class Solution implements Serializable {
 		operationsMachineAssignments = new int[0][];
 		operationsBeginTimes = new int[0][];
 		bids = new Bid[0];
-		problem = null;
+		objectiveValue = Double.NEGATIVE_INFINITY;
 	}
 
-	public Solution(final FJSSTTproblem problem) {
-		this(problem, Double.NEGATIVE_INFINITY);
+	public Solution(final double objectiveValue, final int machines, final int timeslots, final int jobs, final int maxOperationsPerJob) {
+		this(objectiveValue, machines, timeslots, jobs, maxOperationsPerJob, null, 0, new int[machines][timeslots]);
 	}
 
-	public Solution(final FJSSTTproblem problem, final double objectiveValue) {
-		this(problem, objectiveValue, null, 0, new int[problem.getMachines()][problem.getTimeSlots()]);
-	}
-
-	public Solution(final FJSSTTproblem problem, final double objectiveValue, final Bid[] bids, final int iteration,
+	public Solution(final double objectiveValue, final int machines, final int timeslots, final int jobs, final int maxOperationsPerJob, final Bid[] bids, final int iteration,
 			final int[][] subgradients) {
-		this(problem, objectiveValue, bids, iteration, subgradients, null);
+		this(objectiveValue, machines, timeslots, jobs, maxOperationsPerJob, bids, iteration, subgradients, null);
 	}
 
-	public Solution(final FJSSTTproblem problem, final double objectiveValue, final Bid[] bids, final int iteration,
+	public Solution(final double objectiveValue, final int machines, final int timeslots, final int jobs, final int maxOperationsPerJob, final Bid[] bids, final int iteration,
 			final int[][] subgradients, final double[][] multipliers) {
-		this.problem = problem;
 		this.objectiveValue = objectiveValue;
 		this.bids = bids;
 		this.iteration = iteration;
 		this.subgradients = subgradients;
+		this.jobs = jobs;
+		this.machines = machines;
+		this.timeslots = timeslots;
+		this.maxOperationsPerJob = maxOperationsPerJob;
 
-		this.operationsBeginTimes = new int[problem.getJobs()][problem.getMaxOperations()];
-		this.operationsMachineAssignments = new int[problem.getJobs()][problem.getMaxOperations()];
-		this.multipliers = new double[problem.getMachines()][problem.getTimeSlots()];
+		this.operationsBeginTimes = new int[jobs][maxOperationsPerJob];
+		this.operationsMachineAssignments = new int[jobs][maxOperationsPerJob];
+		this.multipliers = new double[machines][timeslots];
 
 		// for all jobs and operations: compile the arrays for optimal machine
 		// assignments and completion times for operations
@@ -102,12 +118,10 @@ public class Solution implements Serializable {
 				System.arraycopy(machineAssignments, 0, this.operationsMachineAssignments[job], 0, machineAssignments.length);
 				System.arraycopy(beginTimes, 0, this.operationsBeginTimes[job], 0, beginTimes.length);
 			}
-		} else {
-			createInitialInfeasibleSolution();
-		}
+		} 
 
 		if (multipliers != null) {
-			for (int m = 0; m < problem.getMachines(); m++) {
+			for (int m = 0; m < machines; m++) {
 				System.arraycopy(multipliers[m], 0, multipliers[m], 0, multipliers[m].length);
 			}
 		}
@@ -120,13 +134,12 @@ public class Solution implements Serializable {
 	 * subgradients are not important for ListScheduling and - to my knowledge-
 	 * need not be set.
 	 */
-	public Solution(final FJSSTTproblem problem, final double objectiveValue, final int[][] begTimes, final int[][] machAss) {
-		this(problem, objectiveValue, begTimes, machAss, 0);
+	public Solution(final double objectiveValue, final int[][] begTimes, final int[][] machAss) {
+		this(objectiveValue, begTimes, machAss, 0);
 	}
 
-	public Solution(final FJSSTTproblem problem, final double objectiveValue, final int[][] begTimes, final int[][] machAss,
+	public Solution(final double objectiveValue, final int[][] begTimes, final int[][] machAss,
 			final int iteration) {
-		this.problem = problem;
 		this.objectiveValue = objectiveValue;
 		this.bids = null;
 		this.iteration = iteration;
@@ -135,11 +148,10 @@ public class Solution implements Serializable {
 		this.operationsMachineAssignments = machAss;
 		this.subgradients = null;
 		this.multipliers = null;
-		createInitialInfeasibleSolution();
 	}
 
 	public Solution clone() {
-		Solution result = new Solution(problem, objectiveValue, bids, iteration, subgradients);
+		Solution result = new Solution(objectiveValue, machines, timeslots, jobs, maxOperationsPerJob, bids, iteration, subgradients);
 		if (operationsBeginTimes != null)
 			for (int i = 0; i < operationsBeginTimes.length; ++i)
 				for (int ii = 0; ii < operationsBeginTimes[i].length; ++ii)
@@ -152,43 +164,7 @@ public class Solution implements Serializable {
 		return result;
 	}
 
-	/**
-	 * TODO: javadoc!
-	 * 
-	 * @return
-	 */
-	protected Solution createInitialInfeasibleSolution() {
-		if (problem != null && (objectiveValue == -1 || objectiveValue == Double.NEGATIVE_INFINITY
-				|| objectiveValue == Double.POSITIVE_INFINITY || objectiveValue == Double.MAX_VALUE || objectiveValue == Double.MIN_VALUE)) {
 
-			for (int i = 0; i < problem.getJobs(); i++) {
-				for (int j = 0; j < problem.getOperations()[i]; j++) {
-					if (j == 0) {
-						setOperationsBeginTimes(i, j, j);
-						final List<Integer> H_ij = problem.getAltMachines(i, j);
-						Random r = new Random();
-						int machine = r.nextInt(H_ij.size());
-						setOperationsMachineAssignments(i, j, H_ij.get(machine));
-
-					} else {
-						setOperationsBeginTimes(i, j, getOperationsBeginTimes()[i][j - 1]
-								+ problem.getProcessTimes()[i][j - 1][getOperationsMachineAssignments()[i][j - 1]]);
-						final List<Integer> H_ij = problem.getAltMachines(i, j);
-						Random r = new Random();
-						int machine = r.nextInt(H_ij.size());
-						setOperationsMachineAssignments(i, j, H_ij.get(machine));
-					}
-				}
-				// H_ij.iterator().next() simply takes the first one. code below
-				// also; no need to create iterator
-				// mInfeasibleSolution.setmOperationsMachineAssignments(i, j,
-				// H_ij.iterator().next());
-
-			}
-
-		}
-		return this;
-	}
 
 	/**
 	 * Checks if two solutions are equal. The check only considers machine
@@ -199,8 +175,8 @@ public class Solution implements Serializable {
 	 * @return True if the solutions are equal.
 	 */
 	public boolean equals(Solution sol) {
-		for (int i = 0; i < problem.getJobs(); i++) {
-			for (int j = 0; j < problem.getOperations()[i]; j++) {
+		for (int i = 0; i < jobs; i++) {
+			for (int j = 0; j < operationsMachineAssignments[i].length; j++) {
 				if (this.operationsMachineAssignments[i][j] != sol.getOperationsMachineAssignments()[i][j])
 					return false;
 				if (this.operationsBeginTimes[i][j] != sol.getOperationsBeginTimes()[i][j])
@@ -275,7 +251,6 @@ public class Solution implements Serializable {
 	@Override
 	public String toString() {
 		return "Solution{" +
-				"problem=" + problem +
 				", objectiveValue=" + objectiveValue +
 				", bids=" + Arrays.toString(bids) +
 				", operationsBeginTimes=" + Arrays.toString(operationsBeginTimes) +
