a79fc2368922a019b15fa7a147f83ecb614c2f0b
gw <georg.weichhart@jku.at>
2016-09-13 16:43:05 +0200

    formatting of test data + test case files

---
+++ src/test/java/at/ngmpps/fjsstt/AsyncActorTriggerTest.java
@@ -0,0 +1,19 @@
+package at.ngmpps.fjsstt;
+
+import at.ngmpps.fjsstt.factory.ModelFactory;
+import at.profactor.NgMPPS.TestRuns.TestAsyncRequests;
+
+public class AsyncActorTriggerTest {
+
+	public static void main(String args[]) throws Exception {
+		TestAsyncRequests test = new TestAsyncRequests();
+		test.parseStrings(ModelFactory.createSrfgProblemSet().getFjs(), 
+				ModelFactory.createSrfgProblemSet().getProperties(),
+				ModelFactory.createSrfgProblemSet().getTransport());
+		
+		// starts a new thread that starts the Akka Actor System
+		test.startAsync();
+		// starts a new thread that polls (~every 10secs) intermediate results and writes to the console
+		test.getCurrSolutionAsync();
+	}
+}
