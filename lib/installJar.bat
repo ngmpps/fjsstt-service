84c09aebfc79ab9df458c9d12d6b7dc923ef55f3
gw <georg.weichhart@jku.at>
2016-09-13 13:31:40 +0200

    integrate maven build

--- lib/installJar.bat
+++ lib/installJar.bat
@@ -1,2 +1,2 @@
-mvn install:install-file -DgroupId=at.ngmpps -DartifactId=DPC -Dversion=1.0-SNAPSHOT -Dpackaging=jar -Dfile=./NgMPPS.jar
+mvn install:install-file -DgroupId=at.ngmpps -DartifactId=DPC -Dversion=1.0-SNAPSHOT -Dpackaging=jar -Dfile=./DPC-1.0-SNAPSHOT.jar
 
