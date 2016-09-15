7cf44ed556e74a19920ffba1cb61ebe93418f0c0
iot365 <fstroh@gmail.com>
2016-09-15 17:17:15 +0200

    fancy logger format.

--- src/main/resources/logback.groovy
+++ src/main/resources/logback.groovy
@@ -1,1 +1,6 @@
-root(WARN)
+appender("Console-Appender", ConsoleAppender) {
+    encoder(PatternLayoutEncoder) {
+        pattern = "%d %highlight(%-5level) [%thread] %cyan(%logger{15}) - %msg %n"
+    }
+}
+root(INFO,["Console-Appender"])
