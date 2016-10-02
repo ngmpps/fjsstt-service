59eea1cf05cf600b8b23b7917dc45b098eabfa0e
iot365 <iot365@users.noreply.github.com>
2016-10-02 18:06:35 +0200

    fixes reading of value in "custom" textareas.

--- src/main/webapp/js/solver.js
+++ src/main/webapp/js/solver.js
@@ -1,9 +1,18 @@
 function initSolver()  {
     $("[id^='post']").click(function(event) {
-        var targetId = event.target.id.split("_")[1]
+        var targetId = event.target.id.split("_")[1];
         var fjsData = $("#fjs_"+targetId)[0].innerText;
+        if ($("#fjs_"+targetId)[0].type === "textarea") {
+            fjsData += $("#fjs_"+targetId)[0].value;
+        }
         var transportData = $("#transport_"+targetId)[0].innerText;
+        if ($("#transport_"+targetId)[0].type === "textarea") {
+            transportData += $("#transport_"+targetId)[0].value;
+        }
         var propertiesData = $("#properties_"+targetId)[0].innerText;
+        if ($("#properties_"+targetId)[0].type === "textarea") {
+            propertiesData += $("#properties_"+targetId)[0].value;
+        }
 
         $.ajax({
               type: "POST",
